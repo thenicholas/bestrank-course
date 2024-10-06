9ccb9dbf7d88a82e37a8d4c1bd58ea9ae363a42f
Nick <nicholas.the.2nd@gmail.com>
2024-10-06 17:20:43 +0300

    feature(task-1): set curl timeouts

--- task-1/php-curl/curl-api-request.php
+++ task-1/php-curl/curl-api-request.php
@@ -6,14 +6,22 @@ $url = 'https://b24-ppqfwe.bitrix24.ru/rest/1/uy41llabw8evqdnx/'.'user.current';
 
 // Initialize a cURL session
 $ch = curl_init();
+
 // I get "self-signed certificate" error without these options
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 curl_setopt($ch, CURLOPT_CAINFO, 'C:\Windows\cacert.pem');
+
 // Set the URL
 curl_setopt($ch, CURLOPT_URL, $url);
+
 // Return the transfer as a string
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Set network timeouts
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);  // Connection timeout in seconds
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);        // Maximum execution time in seconds
+
 // Execute the session and store the result in $response
 $response = curl_exec($ch);
 // Check for cURL errors
